5e244a45a61b1d03e5c8726100dbde91b54a3096
Vejber <105496392+Vejber@users.noreply.github.com>
2023-08-30 18:39:15 +0300

    components and tabs basics

---
+++ blog-components.js
@@ -0,0 +1,12 @@
+Vue.component('change-one',{
+  template: "<h2> change one </h2>"
+});
+
+Vue.component('change-two',{
+    template: "<h2> change two </h2>"
+});
+
+Vue.component('change-three',{
+    template: "<h2> change three </h2>"
+});
+
