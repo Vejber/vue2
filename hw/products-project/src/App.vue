1b9e2a77a94a15beb87901676b042b02dc188ad7
Vejber <105496392+Vejber@users.noreply.github.com>
2023-09-08 16:21:34 +0300

    v-if

---
+++ hw/products-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <ProductDetails/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import ProductDetails from './components/ProductDetails.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    ProductDetails
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
